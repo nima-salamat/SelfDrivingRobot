2d0a840d26a4dfd004b90c7c9c55c3e52f16ba19
ArvinNotDev <Arveenjafari19@gmail.com>
2025-04-11 19:57:47 +0330

    pupil-apriltags to apriltag

--- python/apriltag_detection.py
+++ python/apriltag_detection.py
@@ -1,30 +1,27 @@
 import math
-from pupil_apriltags import Detector
+import apriltag
 import cv2
 from config import TAG_LABLES, MIN_SIZE_APRILTAG
 
 
-
-
-
-detector = Detector(families="tag36h11")
-
+# Initialize the apriltag detector
+detector = apriltag.Detector()
 
 def apriltag_detection(frame):
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect AprilTags in the frame
-    detections = detector.detect(gray, estimate_tag_pose=False)
+    detections = detector.detect(gray)
 
     # Find the nearest apriltag
     main_size = 0
     nearest_apriltag = None
     for detection in detections:
-        corners = [detection.corners.astype(int)]
-        dot1 = [corners[0][0][0], corners[0][0][1]]
-        dot2 = [corners[0][2][0], corners[0][2][1]]
-        size = int(math.dist(dot2, dot1))
+        # Calculate the size of the detected tag (distance between two corners)
+        dot1 = detection.corners[0]
+        dot2 = detection.corners[2]
+        size = int(math.dist(dot1, dot2))
 
         if size >= MIN_SIZE_APRILTAG:
             if size > main_size:
@@ -34,6 +31,7 @@ def apriltag_detection(frame):
     # Show nearest apriltag
     label = "no sign"
     if nearest_apriltag is not None:
+        # Draw the bounding box of the tag
         cv2.polylines(
             frame, [nearest_apriltag.corners.astype(int)], True, (240, 130, 50), 2
         )
