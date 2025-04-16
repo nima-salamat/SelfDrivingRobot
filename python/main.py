34785466aac5bd9a233abc69c32c98dd968a42b0
nima <ssu87556@gmail.com>
2025-04-17 02:09:55 +0330

    add timesleep to detected_crosswalk_bottom

--- python/main.py
+++ python/main.py
@@ -107,6 +107,8 @@ class Robot:
                     print(f"AprilTag detected: {label}")
                     self.last_apriltag = (label, time.time())
                 if detected_crosswalk_bottom:
+                    time.sleep(3)
+
                     self.intersection_navigator.navigate_by_tag(label)
                     continue
                 if self.debug:
