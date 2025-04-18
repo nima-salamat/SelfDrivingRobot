3baafa4c447a5f24f8f0ed00c6615362295c4aa1
nima <ssu87556@gmail.com>
2025-04-18 10:48:10 +0330

    fix relative imports

--- python/tests/test_motor.py
+++ python/tests/test_motor.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from ..modules.serial_connector import connect
 from ..config import ANGLE_CENTER
 arduino = connect()
