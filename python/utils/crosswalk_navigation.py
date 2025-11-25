3a3b9b2cc1fc4076405dfe8f68e1227b03f3bcdb
nima <ssu87556@gmail.com>
2025-11-25 20:00:33 +0330

    handle static navigate arduino commands

--- python/utils/crosswalk_navigation.py
+++ python/utils/crosswalk_navigation.py
@@ -1,20 +1,15 @@
 from config import SPEED
+
+
 class Navigate:
     def __init__(self, controller):
         self.controller = controller
 
     def left(self):
-        self.controller.forward_pulse(SPEED, 5, 90)
-        self.controller.forward_pulse(SPEED, 3, 40)
-        self.controller.forward_pulse(SPEED, 2, 90)
-        
-        
+        self.controller.forward_pulse(f'f {SPEED} 7 90 f {SPEED} 5 40')
         
     def right(self):
-        self.controller.forward_pulse(SPEED, 3, 90)
-        self.controller.forward_pulse(SPEED, 2, 130)
-        self.controller.forward_pulse(SPEED, 1, 90)
-        
-    
+        self.controller.forward_pulse(f'f {SPEED} 5 90 f {SPEED} 5 140')
+
     def straight(self):
-        self.controller.forward_pulse(SPEED, 7, 90)
+        self.controller.forward_pulse(f'f {SPEED} 10 90')
