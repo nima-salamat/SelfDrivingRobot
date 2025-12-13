47d91e067f356666708b53e8a601309721b35a72
nima <ssu87556@gmail.com>
2025-12-13 22:29:24 +0330

    add shutdown route for clean up the stream server

--- python/stream.py
+++ python/stream.py
@@ -1,18 +1,19 @@
 import logging
 import cv2
 import time
-from flask import Flask, Response
+from flask import Flask, Response, request
+import threading
 import config_city
 logger = logging.getLogger(__name__)
 
+stop_event = threading.Event()
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def video_feed():
     def generate():
-        while True:
+        while not stop_event.is_set():
             if config_city.debug_frame_buffer is None:
                 time.sleep(0.01)
                 continue
@@ -23,8 +24,14 @@ def video_feed():
             time.sleep(0.01)
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route("/shutdown", methods=["POST"])
+def shutdown_route():
+    stop_event.set()
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func:
+        func()
+    logger.debug("Server shutting down...")
+    return "Server shutting down..."
+
 def start_stream():
     app.run(host='0.0.0.0', port=5000, threaded=True, debug=False)
-
-    
-
